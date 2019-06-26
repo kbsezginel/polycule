4eb45cfee443db837e7000af0f64ff6d346173d1
kbsezginel <kbs37@pitt.edu>
2019-06-25 23:20:44 -0400

    Rack: cleanup script

--- moduled/moduled_rack/moduled_rack.ino
+++ moduled/moduled_rack/moduled_rack.ino
@@ -1,62 +1,49 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
-
+// ----------------------------------------------------------------------------------
+// POLYCULE | MODULED Rack | Kutay B. Sezginel | June 2019
+// ----------------------------------------------------------------------------------
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_NeoPixel.h>
-
+// ----------------------------------- GLOBALS --------------------------------------
+int gBpm = 120;
+float gDelayTime = 60.0 / gBpm * 1000.0;
+int gDelayTimeInt = gDelayTime;
+// ---------------------------------- OLED SETUP ------------------------------------
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
-// Large LED Ring
-const byte LED_RING1_PIN = 12;
-const byte NUM_PIXELS1 = 24;
-Adafruit_NeoPixel ledRing1 = Adafruit_NeoPixel(NUM_PIXELS1, LED_RING1_PIN, NEO_GRB + NEO_KHZ800);
-
-// Small LED Ring
-const byte LED_RING2_PIN = 11;
-const byte NUM_PIXELS2 = 12;
-Adafruit_NeoPixel ledRing2 = Adafruit_NeoPixel(NUM_PIXELS2, LED_RING2_PIN, NEO_GRB + NEO_KHZ800);
-
-
+unsigned long gDisplayTime = 0.0;
+unsigned long gDisplayDelta = gDelayTime;
+// ------------------------------ LARGE LED RING SETUP ------------------------------
+const byte LARGE_LED_RING_PIN = 12;
+const byte NUM_PIXELS_LARGE = 24;
+Adafruit_NeoPixel ledRing1 = Adafruit_NeoPixel(NUM_PIXELS_LARGE, LARGE_LED_RING_PIN, NEO_GRB + NEO_KHZ800);
+unsigned long gLargeRingDelta = gDelayTime;
+// ------------------------------ SMALL LED RING SETUP ------------------------------
+const byte SMALL_LED_RING_PIN = 11;
+const byte NUM_PIXELS_SMALL = 12;
+Adafruit_NeoPixel ledRing2 = Adafruit_NeoPixel(NUM_PIXELS_SMALL, SMALL_LED_RING_PIN, NEO_GRB + NEO_KHZ800);
+unsigned long gSmallRingDelta = gDelayTime;
 
 byte gColorIdx = 0;
 byte gColorStep = 9;
 byte gLedStep = 3;
-
-// KNOBS
-// KNOB 1 - Led Ring Step Size
-// KNOB 2 - Led Ring Color Step Size
-// KNOB 3 - BPM and multiplier
-// SWITCH
-// Between state 1 and 2
-
+byte gLedCounter = 0;
+unsigned long gCurrentTime = 0.0;
+// ----------------------------- POTENTIOMETER SETUP --------------------------------
 const byte LEFT_POT_PIN = 0;
 const byte RIGHT_POT_PIN = 1;
-
-const byte BUTTON_PIN = 3;
-const byte SWITCH_PIN = 4;
-
-int bpm = 120;
 int gLeftPotVal = 500;
 int gRightPotVal = 500;
-float delayTime = 60.0 / bpm * 1000.0;
-int delayTimeInt = delayTime;
-byte ledCounter = 0;
-unsigned long currentTime = 0.0;
-unsigned long displayTime = 0.0;
-unsigned long displayDelta = delayTime;
-
-unsigned long timeDelta1 = delayTime;
-unsigned long timeDelta2 = delayTime;
-
-
+// -------------------------------- SWITCH SETUP ------------------------------------
+const byte SWITCH_PIN = 4;
+// -------------------------------- BUTTON SETUP ------------------------------------
+const byte BUTTON_PIN = 3;
+// ----------------------------------------------------------------------------------
+// >x< SETUP >x<
+// ----------------------------------------------------------------------------------
 void setup() {
   pinMode(SWITCH_PIN, INPUT);
   pinMode(BUTTON_PIN, INPUT);
@@ -66,20 +53,17 @@ void setup() {
 
   ledRing2.begin();
   ledRing2.setBrightness(50);  // btw 0 - 127
-  
+
   Serial.begin(115200);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
-  
-  display.clearDisplay();
 
+  display.clearDisplay();
   display.setTextSize(2);
-  display.setTextColor(WHITE);
   display.setCursor(20, 20);
-  // Display static text
   display.println("MODULED!");
   display.display();
   delay(1000);
@@ -90,22 +74,22 @@ void loop() {
   setColor();
   printDisplay();
 
-  if (millis() - currentTime > timeDelta1){
-    lightLedRing1(0, ledCounter, gColorIdx);
+  if (millis() - gCurrentTime > gSmallRingDelta){
+    lightLedRing1(0, gLedCounter, gColorIdx);
   }
-  if (millis() - currentTime > timeDelta2){
-    lightLedRing2(0, ledCounter, gColorIdx);
-    ledCounter += gLedStep;
+  if (millis() - gCurrentTime > gLargeRingDelta){
+    lightLedRing2(0, gLedCounter, gColorIdx);
+    gLedCounter += gLedStep;
     gColorIdx += gColorStep * gLedStep;
-    currentTime = millis();
+    gCurrentTime = millis();
   }
 
-  if (ledCounter > NUM_PIXELS1) {
+  if (gLedCounter > NUM_PIXELS_LARGE) {
     clearLedRing1();
     clearLedRing2();
-    ledCounter = gLedStep;
+    gLedCounter = gLedStep;
     gColorIdx = 0;
-    currentTime = millis();
+    gCurrentTime = millis();
   }
 }
 
@@ -113,9 +97,9 @@ void setBPM() {
   int potRead = analogRead(LEFT_POT_PIN);
   // Change value only if current read is different than previous set value
   if (abs(gLeftPotVal - potRead) > 20) {
-    bpm = map(potRead, 0, 1023, 10, 300);
-    delayTime = 60.0 / bpm * 1000.0;
-    delayTimeInt = delayTime;
+    gBpm = map(potRead, 0, 1023, 10, 300);
+    gDelayTime = 60.0 / gBpm * 1000.0;
+    gDelayTimeInt = gDelayTime;
   }
 }
 
@@ -134,12 +118,12 @@ void printDisplay(){
   display.println(" BPM | DLY");
   display.setTextSize(2);
   display.setCursor(10, 20);
-  display.println(bpm);
+  display.println(gBpm);
   display.setCursor(85, 20);
-  display.println(delayTimeInt);
+  display.println(gDelayTimeInt);
   display.setCursor(0, 40);
   display.setTextSize(1);
-  int beatNum = ceil(millis() - displayTime) / displayDelta;
+  int beatNum = ceil(millis() - gDisplayTime) / gDisplayDelta;
   if (beatNum == 1){
     display.println(">                   <");
   } else if (beatNum == 2) {
@@ -149,7 +133,7 @@ void printDisplay(){
   } else if (beatNum == 4) {
     display.println("> > > >   .   < < < <");
   } else if (beatNum > 4) {
-    displayTime = millis();
+    gDisplayTime = millis();
   }
   display.display();
 }
@@ -158,7 +142,7 @@ void printDisplay(){
 // LED RING FUNCTIONS
 // ----------------------------------------------------------------------------------
 void clearLedRing1() {
-  for(int i = 0; i<NUM_PIXELS1; i++){
+  for(int i = 0; i<NUM_PIXELS_LARGE; i++){
     ledRing1.setPixelColor(i, 0x000000);
     ledRing1.show();
   }
@@ -190,10 +174,10 @@ uint32_t dimColor(uint32_t color, uint8_t width) {
 
 // LED Animation (KnightRider)
 void ledAnim1(uint16_t cycles, uint16_t speed, uint8_t width, uint32_t color, bool clearAll) {
-  uint32_t old_val[NUM_PIXELS1]; // up to 256 lights!
+  uint32_t old_val[NUM_PIXELS_LARGE]; // up to 256 lights!
   // Larson time baby!
   for(int i = 0; i < cycles; i++){
-    for (int count = 0; count<NUM_PIXELS1 + 1; count++) {
+    for (int count = 0; count<NUM_PIXELS_LARGE + 1; count++) {
       ledRing1.setPixelColor(count, color);
       old_val[count] = color;
       for(int x = count; x>0; x--) {
@@ -203,10 +187,10 @@ void ledAnim1(uint16_t cycles, uint16_t speed, uint8_t width, uint32_t color, bo
       ledRing1.show();
       delay(speed);
     }
-   for (int count = NUM_PIXELS1-1; count>=0; count--) {
+   for (int count = NUM_PIXELS_LARGE-1; count>=0; count--) {
      ledRing1.setPixelColor(count, color);
      old_val[count] = color;
-     for(int x = count; x<=NUM_PIXELS1 ;x++) {
+     for(int x = count; x<=NUM_PIXELS_LARGE ;x++) {
        old_val[x-1] = dimColor(old_val[x-1], width);
        ledRing1.setPixelColor(x+1, old_val[x+1]);
      }
@@ -223,7 +207,7 @@ void ledAnim1(uint16_t cycles, uint16_t speed, uint8_t width, uint32_t color, bo
 // LED RING FUNCTIONS
 // ----------------------------------------------------------------------------------
 void clearLedRing2() {
-  for(int i = 0; i<NUM_PIXELS2; i++){
+  for(int i = 0; i<NUM_PIXELS_SMALL; i++){
     ledRing2.setPixelColor(i, 0x000000);
     ledRing2.show();
   }
@@ -238,10 +222,10 @@ void lightLedRing2(int startIndex, int nPixels, int colorIdx) {
 
 // LED Animation (KnightRider)
 void ledAnim2(uint16_t cycles, uint16_t speed, uint8_t width, uint32_t color, bool clearAll) {
-  uint32_t old_val[NUM_PIXELS2]; // up to 256 lights!
+  uint32_t old_val[NUM_PIXELS_SMALL]; // up to 256 lights!
   // Larson time baby!
   for(int i = 0; i < cycles; i++){
-    for (int count = 0; count<NUM_PIXELS2 + 1; count++) {
+    for (int count = 0; count<NUM_PIXELS_SMALL + 1; count++) {
       ledRing2.setPixelColor(count, color);
       old_val[count] = color;
       for(int x = count; x>0; x--) {
@@ -251,10 +235,10 @@ void ledAnim2(uint16_t cycles, uint16_t speed, uint8_t width, uint32_t color, bo
       ledRing2.show();
       delay(speed);
     }
-   for (int count = NUM_PIXELS2-1; count>=0; count--) {
+   for (int count = NUM_PIXELS_SMALL-1; count>=0; count--) {
      ledRing2.setPixelColor(count, color);
      old_val[count] = color;
-     for(int x = count; x<=NUM_PIXELS2 ;x++) {
+     for(int x = count; x<=NUM_PIXELS_SMALL ;x++) {
        old_val[x-1] = dimColor(old_val[x-1], width);
        ledRing2.setPixelColor(x+1, old_val[x+1]);
      }
